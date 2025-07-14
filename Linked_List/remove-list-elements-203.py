9e3ae932eb204b384a846db8b1804b22488ac9b0
Pankaj9607 <pankajdahiwalkar@gmail.com>
2025-07-15 00:49:28 +0530

    Added function to remove multiple elements from list

---
+++ Linked_List/remove-list-elements-203.py
@@ -0,0 +1,32 @@
+'''
+Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+
+Example 1:
+Input: head = [1,2,6,3,4,5,6], val = 6
+Output: [1,2,3,4,5]
+
+Example 2:
+Input: head = [], val = 1
+Output: []
+
+Example 3:
+Input: head = [7,7,7,7], val = 7
+Output: []
+'''
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        current = dummy
+
+        while current:
+            while current.next and current.next.val == val:
+                current.next = current.next.next
+            current = current.next
+
+        return dummy.next
